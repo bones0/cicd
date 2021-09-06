6a7d90a4025ffff3ac909f03625b5241520082c9
Guhl <peter.guhl@unisg.ch>
2021-09-06 17:11:59 +0200

    Reversed changes, add container names

--- get-scs-base.sh
+++ get-scs-base.sh
@@ -10,4 +10,3 @@ cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 curl https://get.docker.com | sh && sudo systemctl --now enable docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo docker network create scs-asse
